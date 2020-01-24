feb84a507986464056468c92bf55281782d090d3
Phuffick <sheklokow@gmail.com>
2020-01-24 21:09:09 +0300

    Stack v1

---
+++ classes.h
@@ -0,0 +1,113 @@
+#pragma once
+#include <iostream>
+
+
+class Node {
+
+	friend class Stack;
+
+private:
+
+	int d;
+	Node* next;
+	int key;
+
+public:
+
+	Node(int d, Node* next) {
+
+		this->next = next;
+		this->d = d;
+	};
+
+	Node(int d) {
+	
+		this->d = d;
+	};
+
+	~Node() { delete next; }
+};
+
+class Stack {
+
+private:
+
+	Node* begin;
+
+public:
+
+	Stack() : begin(NULL) {};
+
+	~Stack() { delete begin; };
+
+	void append(int d) {
+
+		Node* New = new Node(d);
+	
+		if (begin) {
+
+			bool flag = true;
+			Node* curr = begin;
+			while (curr && flag) {
+
+				if (!(curr->next)) {
+					curr->next = New;
+					flag = false;
+				}
+				curr = curr->next;
+			}
+		}
+		else {
+			begin = New;
+		}
+
+		Counter();
+	};
+
+	void pop() {
+	
+		if (begin) {
+
+			Node* curr = begin;
+			while (curr) {
+
+				if (!((curr->next)->next))
+					curr->next = NULL;
+				curr = curr->next;
+			}
+		}
+		else
+			std::cout << "It is already clear. " << std::endl;
+
+		Counter();
+	};
+
+	void Print() {
+	
+		if (begin) {
+
+			Node* curr = begin;
+			while (curr) {
+
+				std::cout << curr->key << " - " << curr->d << std::endl;
+				curr = curr->next;
+			}
+		}
+		else {
+		
+			std::cout << "It is clear. " << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	void Counter() {
+
+		int i = 0;
+		Node* curr = begin;
+		while (curr)
+		{
+			curr->key = ++i;
+			curr = curr->next;
+		}
+	};
+};
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "classes.h"
+
+
+int main() {
+
+	Stack st;
+	st.append(1);
+	st.append(2);
+	st.append(3);
+	st.append(4);
+	st.append(5);
+
+	st.Print();
+
+	st.pop();
+	st.pop();
+	st.pop();
+
+	st.Print();
+
+	system("pause");
+	return 0;
+}
